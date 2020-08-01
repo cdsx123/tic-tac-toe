c571ec6e76c877fe0d21a24b31a9a017b330569f
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2020-08-01 16:49:18 -0700

    created the tic tac toe square

--- script.js
+++ script.js
@@ -13,14 +13,36 @@ const gameBoard = (() => {
 
     
 
-    }
+    return {getBoard, resetBoard, getBoardSquare}
 
 })();
 
 const game = (() => {
-    const render = () = {
-        
-    }
-    
-})();
+    const board = document.querySelector('#board')
+    marker = 'X'
 
+    const render = () => {
+        
+        const squareWidth = 338/3;
+        const squareHeight = 340/3;
+        
+        for(let i = 0; i < 9; i++){
+            const square = document.createElement('div')
+            square.style.cssText = `width: ${squareWidth}px; height: ${squareHeight}px; border: 2px solid black; 
+            font-size: 115px; display: flex; justify-content: center; align-items: center; font-family: arial;
+            `
+            square.textContent = 'X'
+            board.appendChild(square)
+            
+        }
+    }
+    board.addEventListener('click', (e) => {
+        if (e.target.textContent === ''){
+        e.target.textContent = marker
+        // put player switch function here
+        }
+    })
+
+    return {render}
+})();
+game.render()
