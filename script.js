270b661a1502a8d1946a280c282222702cb5f1af
cdsx123 <ekamsinghsidhu@yahoo.com>
2020-08-01 13:42:46 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const Player = (name, shape) => {
+    return {name, shape};
+}
+
+const gameBoard = (() => {
+    let board = ['','','','','','','','',''];
+
+    const getBoard = () => board;
+
+    const resetBoard = () => board = ['','','','','','','','',''];
+
+    const getBoardSquare = (cellID) => board[cellID];
+
+    
+
+    }
+
+})();
+
+const game = (() => {
+    const render = () = {
+        
+    }
+    
+})();
+
